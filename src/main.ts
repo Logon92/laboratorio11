f7dee8d7b8f8d5f25af98ae07a604f32e2ef491d
Logon92 <143187203+Logon92@users.noreply.github.com>
2025-08-14 20:52:10 +0200

    save

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { pintarExtraerBtn } from './ui';
 document.addEventListener('DOMContentLoaded', () => {
     pintarExtraerBtn();
     document.getElementById("validarBtn")?.addEventListener("click", () => {
-        const input = (document.getElementById("floatingInput") as HTMLInputElement).value;
-        validarIBAN(input);
+        const inputElement = document.getElementById("floatingInput");
+        if (inputElement instanceof HTMLInputElement) {
+            const input = inputElement.value;
+            validarIBAN(input);
+        } else {
+            console.error("El elemento no es un input HTML válido");
+        }
     });
 });
